94fd0045a6300a05833d9baa173679c20ee4836c
Sebastian Santos <santossebastian268@gmail.com>
2024-08-07 17:14:56 -0400

    funcion buscar id y añadir al carrito

--- practice.py
+++ practice.py
@@ -1,5 +1,5 @@
 import os
-
+precio = 0
 productos = [
 {"id": 1,"producto":"arroz","precio":50 },
 {"id": 2,"producto":"habichuelas","precio":80 },
@@ -22,4 +22,70 @@ def menu(productos):
             6. Salir
             ''')
         opc=input("Elige una de las opciones: ")
+        if opc=="1":
+            id=1
+            cantidad = int(input("Cuantas lbs de arroz quieres: "))
+            if cantidad<=0:
+                print("No se puede ingresar una cantidad negativa, ni tampoco 0")
+                input ("Presione enter para volver al menu: ")
+            precio_tot = 50 * cantidad
+            agregar_al_carrito (id,productos,cantidad,precio_tot)
+        elif opc=="2": 
+            id=2
+            cantidad = int(input("Cuantas lbs de habichuelas quieres: "))
+            if cantidad<=0:
+                print("No se puede ingresar una cantidad negativa, ni tampoco 0")
+                input ("Presione enter para volver al menu: ")
+            precio_tot = 80 * cantidad
+            agregar_al_carrito (id,productos,cantidad,precio_tot)
+        elif opc=="3":
+            id=3
+            cantidad = int(input("Cuantas lbs de aceite quieres: "))
+            if cantidad<=0:
+                print("No se puede ingresar una cantidad negativa, ni tampoco 0")
+                input ("Presione enter para volver al menu: ")
+            precio_tot = 300 * cantidad
+            agregar_al_carrito (id,productos,cantidad,precio_tot)
+        elif opc=="4":
+            id=4
+            cantidad = int(input("Cuantas lbs de pollo quieres: "))
+            if cantidad<=0:
+                print("No se puede ingresar una cantidad negativa, ni tampoco 0")
+                input ("Presione enter para volver al menu: ")
+            precio_tot = 85 * cantidad
+            agregar_al_carrito (id,productos,cantidad,precio_tot)
+        elif opc=="5":
+            id=5
+            cantidad = int(input("Cuantas lbs de lechuga quieres: "))
+            if cantidad<=0:
+                print("No se puede ingresar una cantidad negativa, ni tampoco 0")
+                input ("Presione enter para volver al menu: ")
+                precio_tot = 80 * cantidad
+            agregar_al_carrito (id,productos,cantidad,precio_tot)
+        elif opc=="6":
+            print ("Adios")
+            print (carrito)
+
+def buscar_producto(id, productos):
+    for producto in productos:
+        if producto['id']==int(id):
+            return producto
+    return None
+
+def agregar_al_carrito (id,productos,cantidad,precio_tot):
+    producto = buscar_producto(id,productos)
+    if producto == None:
+        print ("El producto seleccionado no existe")
+    elif producto != None:
+        if id == 1:
+            carrito.append({"id": 1,"producto":"arroz","precio":50,"cantidad":cantidad,"precio total":precio_tot})
+        elif id == 2:
+            carrito.append({"id": 2,"producto":"habichuelas","precio":80,"cantidad":cantidad,"precio total":precio_tot})
+        elif id == 3:
+            carrito.append({"id": 3,"producto":"aceite","precio":300,"cantidad":cantidad,"precio total":precio_tot})
+        elif id == 4:
+            carrito.append({"id": 4,"producto":"pollo","precio":85,"cantidad":cantidad,"precio total":precio_tot})
+        elif id == 5:
+            carrito.append({"id": 5,"producto":"lechuga","precio":80,"cantidad":cantidad,"precio total":precio_tot })
             
+print(menu(productos))
